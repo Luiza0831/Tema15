6cf5072aebda84de09ee61630d92e1e5e7d36b10
Luiza Alexandru <alexandru97luiza@yahoo.com>
2024-05-23 14:29:05 +0300

    am modificat fisierul modificari.py dupa cerinta problemei 6

--- modificari.py
+++ modificari.py
@@ -1,9 +1,9 @@
-import csv
+import csv,json
 # 5. Scrieti o functie care va mari cu 1 varsta tuturor persoanelor din fisierul csv
 calecsv="Lista_json_to_csv.csv"
 def incrementez_varsta_din_csv():
     listacsv=[]
-    with open(calecsv,'r')as csvfile:
+    with open(calecsv,'r') as csvfile:
         reader=csv.reader(csvfile)
         for line in reader:
             header=line
@@ -17,3 +17,15 @@ def incrementez_varsta_din_csv():
         writer.writerows(listacsv)
 
 incrementez_varsta_din_csv()
+
+# 6. Scrieti o functie care va mari cu 1 varsta tuturor persoanelor dinf fisierul json
+calejson="Lista_Nume.json"
+def incrementez_varsta_din_json():
+    with open(calejson,'r') as jsonfile:
+        listajson=json.load(jsonfile)
+        for dict in listajson:
+            dict["Varsta"]+=1
+    with open(calejson,'w') as jsonfile:
+        json.dump(listajson,jsonfile)
+
+incrementez_varsta_din_json()
